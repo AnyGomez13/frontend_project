d8709d71fb8b01282f92094e182eae61f262f83b
Jorshua Hernandez <jorshua321@gmail.com>
2023-11-15 14:05:06 -0500

    ajustes login y falta finalizar add sesion

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,19 @@
 import "./App.css";
-import { Routes, Route } from "react-router-dom"; // Asegúrate de que Routes se importe correctamente.
+import {Routes, Route, Navigate} from "react-router-dom"; // Asegúrate de que Routes se importe correctamente.
 import Login from "./components/Login";
 import Cuestionario from "./components/Cuestionario";
 
 function App() {
-  return (
-    <Routes>
-      <Route path="/" element={<Login />} />
-      <Route path="/cuestionario" element={<Cuestionario />} />
-    </Routes>
-  );
-
-  // return(
-  //   <>
-  //     <Login/>    
-  //   </>
-  // )
+    return (
+        <Routes>
+            <Route path="/" element={<Login/>}/>
+            <Route
+                path="/login"
+                element={<Navigate to="/" replace/>}
+            />
+            <Route path="/cuestionario" element={<Cuestionario/>}/>
+        </Routes>
+    );
 }
+
 export default App;
