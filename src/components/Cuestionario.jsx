d849181a393fa45ab7e8da1faca4c3a8a9d83a62
Any Gomez <anymelisa613@gmail.com>
2023-11-15 06:44:59 -0500

    cositas

--- src/components/Cuestionario.jsx
+++ src/components/Cuestionario.jsx
@@ -5,9 +5,11 @@ export default function CrearPreguntas(){
 
   return(
     <div className="contenedorPrincipal">
+      <div className="contenedorArriba"></div>
       <div className="contenedorIzquierda"></div>
-      <div className="contenedorDerecha"></div>
-      <div className="contenedorSapo"></div>
+      <div className="contenedorDerecha">
+          <h1>Cuestionarios</h1>
+      </div>
     </div>
   )
 };
